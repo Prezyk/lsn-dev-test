7ce9212e771e53a103b8bba4dbc601761c9fc880
Kacper Perz <kacper.perz@marviq.com>
2021-07-21 20:28:26 +0200

    Task1 - logic for sorting and duplicates removal implemented

--- src/main/java/task1/process/IntegerListProcessorImpl.java
+++ src/main/java/task1/process/IntegerListProcessorImpl.java
@@ -1,12 +1,33 @@
 package task1.process;
 
 import task1.data.DistinctSortedElements;
+import task1.data.DistinctSortedElementsBuilder;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class IntegerListProcessorImpl implements IntegerListProcessor{
     @Override
     public DistinctSortedElements processIntegerList(List<Integer> integerList) {
-        return null;
+        DistinctSortedElements distinctSortedElements = null;
+        if(integerList != null && integerList.size()>0) {
+            List<Integer> integerElements = new ArrayList<>(new HashSet<>(integerList));
+            integerElements.sort(Integer::compare);
+            distinctSortedElements = createResult(integerElements, integerList.size());
+        }
+        return distinctSortedElements;
+    }
+
+    private DistinctSortedElements createResult(List<Integer> sortedElements, int originalSize) {
+        DistinctSortedElements distinctSortedElements = new DistinctSortedElementsBuilder()
+                .setElements(sortedElements)
+                .setMinValue(sortedElements.get(0))
+                .setMaxValue(sortedElements.get(sortedElements.size()-1))
+                .setElementCount(originalSize)
+                .setDistinctElementCount(sortedElements.size())
+                .createDistinctSortedElements();
+        return distinctSortedElements;
     }
 }
