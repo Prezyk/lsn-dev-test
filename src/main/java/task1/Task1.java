005e56bc9f6653eef3b2ab85dff31ec8f66403d5
Kacper Perz <kacper.perz@marviq.com>
2021-07-21 16:32:35 +0200

    Added general application flow

---
+++ src/main/java/task1/Task1.java
@@ -0,0 +1,24 @@
+package task1;
+
+import task1.data.DistinctSortedElements;
+
+import java.util.Scanner;
+
+public class Task1 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String userInput = scanner.nextLine();
+        DistinctSortedElements processResult = processInput(userInput);
+        printFormattedResult(processResult);
+    }
+
+    private static DistinctSortedElements processInput(String input) {
+        return null;
+    }
+
+    private static void printFormattedResult(DistinctSortedElements distinctSortedElements) {
+
+    }
+
+}
