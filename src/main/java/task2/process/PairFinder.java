9354024064dbdee1b80226ea19c12f32c61eb10a
Kacper Perz <kacper.perz@marviq.com>
2021-07-21 21:31:49 +0200

    Task2 - Pair finder interface added

---
+++ src/main/java/task2/process/PairFinder.java
@@ -0,0 +1,7 @@
+package task2.process;
+
+import java.util.List;
+
+public interface PairFinder {
+    List<Integer[]> findPairs(List<Integer> integers, int sumUpTo);
+}
---
+++ src/main/java/task2/process/PairFinderImp.java
@@ -0,0 +1,10 @@
+package task2.process;
+
+import java.util.List;
+
+public class PairFinderImp implements PairFinder{
+    @Override
+    public List<Integer[]> findPairs(List<Integer> integers, int sumUpTo) {
+        return null;
+    }
+}
