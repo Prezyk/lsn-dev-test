7ad6a321621ee2e464a2a910c5a5bfaead7489d5
Kacper Perz <kacper.perz@marviq.com>
2021-07-22 21:02:28 +0200

    Task2 - refactor and unused imports cleanup

--- src/main/java/task2/process/PairFinderImp.java
+++ src/main/java/task2/process/PairFinderImp.java
@@ -1,10 +1,6 @@
 package task2.process;
 
-import com.sun.org.apache.bcel.internal.generic.LineNumberGen;
-
-import java.lang.reflect.Array;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 
 public class PairFinderImp implements PairFinder {
@@ -35,20 +31,18 @@ public class PairFinderImp implements PairFinder {
 
     private Integer findPairForInteger(Integer integerToPair, List<Integer> integers) {
         Integer pairedInteger = null;
-        int index=integers.size()-1;
-        for(; index>=0; index--) {
+        for(int index=integers.size()-1; index>=0; index--) {
             if(integers.get(index)==null) {
                 continue;
             }
             int pairSum = integers.get(index) + integerToPair;
             if(pairSum == sumUpTo) {
                 pairedInteger = integers.get(index);
+                integers.set(index, null);
                 break;
             }
         }
-        if(pairedInteger!=null) {
-            integers.set(index, null);
-        }
+
         return pairedInteger;
     }
 }
--- src/test/java/task2/process/PairFinderImpTest.java
+++ src/test/java/task2/process/PairFinderImpTest.java
@@ -2,7 +2,6 @@ package task2.process;
 
 import org.junit.jupiter.api.Test;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
