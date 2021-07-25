e0e742b551592ef3303a21e68c6e06ff362f0ee6
Kacper Perz <kacper.perz@marviq.com>
2021-07-26 01:11:22 +0200

    Task3 - Connection parse logic implemented

--- src/main/java/task3/process/ConnectionParserImpl.java
+++ src/main/java/task3/process/ConnectionParserImpl.java
@@ -3,8 +3,35 @@ package task3.process;
 import task3.process.exception.ConnectionParseException;
 
 public class ConnectionParserImpl implements ConnectionParser {
+
+    private static final String VERTICES_SEPARATOR = " ";
+    private static final String INVALID_ELEMENT_COUNT_MESSAGE = "Invalid vertices count at row: %d";
+    private static final String INVALID_CHAR_AT_CONNECTION_ROW = "Invalid vertex char at row: %d";
+
     @Override
     public int[][] parseConnections(String[] connectionsRows) throws ConnectionParseException {
-        return null;
+        int[][] connections = new int[connectionsRows.length][];
+        for(int rowIndex=0; rowIndex<connectionsRows.length; rowIndex++) {
+            connections[rowIndex] = parseConnectionRow(connectionsRows[rowIndex], rowIndex);
+        }
+        return connections;
     }
+
+
+    private int[] parseConnectionRow(String connectionRow, int rowIndex) throws ConnectionParseException {
+        String[] connectionRowElements = connectionRow.split(VERTICES_SEPARATOR);
+        if(connectionRowElements.length!=2) {
+            throw new ConnectionParseException(String.format(INVALID_ELEMENT_COUNT_MESSAGE, rowIndex));
+        }
+        int[] connectionVertices = new int[2];
+        for(int elementIndex=0; elementIndex<connectionRowElements.length; elementIndex++) {
+            try {
+                connectionVertices[elementIndex] = Integer.parseInt(connectionRowElements[elementIndex]);
+            } catch (NumberFormatException numberFormatException) {
+                throw new ConnectionParseException(String.format(INVALID_CHAR_AT_CONNECTION_ROW, rowIndex), numberFormatException);
+            }
+        }
+        return connectionVertices;
+    }
+
 }
--- src/main/java/task3/process/exception/ConnectionParseException.java
+++ src/main/java/task3/process/exception/ConnectionParseException.java
@@ -1,9 +1,11 @@
 package task3.process.exception;
 
 public class ConnectionParseException extends Exception {
-    private static final String MESSAGE = "Invalid string to prepare array of vertices connections.";
+    public ConnectionParseException(String message) {
+        super(message);
+    }
 
-    public ConnectionParseException(Throwable cause) {
-        super(MESSAGE, cause);
+    public ConnectionParseException(String message, Throwable cause) {
+        super(message, cause);
     }
 }
