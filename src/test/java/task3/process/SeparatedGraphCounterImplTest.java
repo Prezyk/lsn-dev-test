bf4e354e924531f907f4741ee9ba258dd1c6d6c4
Kacper Perz <kacper.perz@marviq.com>
2021-07-26 00:50:21 +0200

    Task3 - SeparatedGraphCounterImpl test implemented

---
+++ src/test/java/task3/process/SeparatedGraphCounterImplTest.java
@@ -0,0 +1,23 @@
+package task3.process;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SeparatedGraphCounterImplTest {
+
+    private SeparatedGraphCounter separatedGraphCounter = new SeparatedGraphCounterImpl();
+
+    @Test
+    void testCountSeparatedGraphs() {
+        int expectedNumberOfSeparatedGraphs = 2;
+        int[][] connections = new int[][]{
+                new int[]{2, 3},
+                new int[]{1, 2},
+                new int[]{5, 6}
+        };
+        int actualNumberOfSeparatedGraphs = separatedGraphCounter.countSeparatedGraphs(connections);
+        assertEquals(expectedNumberOfSeparatedGraphs, actualNumberOfSeparatedGraphs);
+    }
+
+}
